42676750794d164b75b7e7a12a9d1c828b44ce08
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-10 21:23:46 -0300

    Questao 1 exercicio do dia -Aquele da foto do doguinho ^^

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      image: '',
+      L: true,
+    };
+  }
+
+  componentDidMount() {
+    this.dog = fetch('https://dog.ceo/api/breeds/image/random')
+      .then((promise) => promise.json())
+      .then((promise) => {
+        this.setState({
+          image: promise.message,
+          L: false,
+        });
+      });
+  }
+
+  render() {
+    const { image, L } = this.state;
+    const Loading = 'Loading nenem...';
+    return (
+      <section>
+        {L ? <p>{ Loading }</p> : <img src={ image } className="App-logo" alt="logo" />}
+        <button type="button" id="dog-btn">New Doguinho</button>
+      </section>
+    );
+  }
 }
 
 export default App;
